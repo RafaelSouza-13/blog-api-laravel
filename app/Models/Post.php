36e72786ee01a5e8cee071169f060f6daf5557f7
Rafael Souza <rafa.nds13@gmail.com>
2025-07-23 12:18:37 -0300

    Criando as tabelas tags, posts_tags e definindo suas relações entre os modelos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,20 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     protected $table = 'posts';
+    public $timestamps = false;
     const UPDATEDAT = 'updatedAt';
     const CREATEDAT = 'createdAt';
 
-    // protected $fillable = [
-    //     'title',
-    //     'content',
-    //     'cover_image',
-    // ];
+    protected $fillable = [
+        'slug',
+        'user_id',
+        'title',
+        'content',
+        'cover_image',
+        'status',
+    ];
 
     public function user(): BelongsTo{
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags(): BelongsToMany{
+        return $this->belongsToMany(Tag::class, 'posts_tags', 'post_id', 'tag_id');
     }
 }
