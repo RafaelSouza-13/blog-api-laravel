f62da53a471238bc18d735aa9afdafb6fdcd4d94
Rafael Souza <rafa.nds13@gmail.com>
2025-07-25 16:31:36 -0300

    Retornando posts com tags relacionadas

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,8 +12,6 @@ class PostController extends Controller
         $posts = Post::select('id', 'slug', 'title', 'body', 'cover_image', 'createdAt', 'user_id')
                     ->with(['user:id,name,email', 'tags:name'])
                     ->paginate(10);
-        
-        
         return response()->json($posts, 200);
     }
 
@@ -25,5 +23,23 @@ class PostController extends Controller
             return response()->json(['message' => 'Post não encontrado'], 404);
         }
         return response()->json($post, 200);
+    }
+
+    public function getRelatedPosts($slug){
+        $post = Post::where('slug', $slug)
+                    ->with(['user:id,name,email', 'tags:id,name'])
+                    ->firstOrFail();
+
+        if (!$post) {
+            return response()->json(['message' => 'Post não encontrado'], 404);
+        }
+
+        $relatedPosts = Post::where('id', '!=', $post->id)
+                            ->whereHas('tags', function($query) use ($post) {
+                                $query->whereIn('id', $post->tags->pluck('id'));
+                            })
+                            ->with(['user:id,name,email', 'tags:name'])->get();
+                            
+        return response()->json($relatedPosts, 200);
     }
 }
